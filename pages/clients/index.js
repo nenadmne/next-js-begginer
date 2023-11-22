27cd0edbf45ad72fbc00b4f67b3c20f7826479be
nenadmne <cosovicnenad14@gmail.com>
2023-11-22 21:18:20 +0100

    Link from next/link

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,3 +1,27 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
-  return <h1> Clients Page </h1>;
+  const clients = [
+    {
+      id: "max",
+      name: "Maximilian",
+    },
+    {
+      id: "manu",
+      name: "Manuel",
+    },
+  ];
+
+  return (
+    <div>
+      <h1> Clients Page </h1>
+      <ul>
+        {clients.map((item) => (
+          <li key={item.id}>
+            <Link href={`/clients/${item.id}`}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function Homepage() {
-  return <h1>Homepage</h1>;
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio"> Portfolio </Link>
+        </li>
+        <li>
+          <Link href="/clients"> Clients </Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
