136caf5d14a85a03e6dafdf1381d2d9287980783
nenadmne <cosovicnenad14@gmail.com>
2023-11-22 21:25:44 +0100

    Navigating programmatically with router.push

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,3 +1,14 @@
+import { useRouter } from "next/router";
+
 export default function ClientProjectPage() {
-  return <h1> The project of a given client</h1>;
+  const router = useRouter();
+  const LoadProjectHandler = () => {
+    router.push("/clients/max/projecta");
+  };
+  return (
+    <div>
+      <h1> The project of a given client</h1>
+      <button onClick={LoadProjectHandler}> Load project a </button>
+    </div>
+  );
 }
